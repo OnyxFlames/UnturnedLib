b031c5d3f250e6be3eb30c197663ddd4ed1df2db
Jacob Gordon <jacobhicks123123@gmail.com>
2021-04-12 18:05:34 -0400

    Attachable barrels

---
+++ Attachments/BarrelAttachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnturnedLib.Attachments
+{
+    public class BarrelAttachment : Attachment
+    {
+        public sbyte BulletRecoil { get; set; }
+        public sbyte BulletSpread { get; set; }
+        public sbyte BulletDrop { get; set; }
+        public sbyte FiringNoise { get; set; }
+        public sbyte XAxis { get; set; }
+        public sbyte YAxis { get; set; }
+        public bool HidesFlash { get; set; }
+        public bool HidesTrails { get; set; }
+    }
+}
--- Identifiable.cs
+++ Identifiable.cs
@@ -9,5 +9,10 @@ namespace UnturnedLib
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
